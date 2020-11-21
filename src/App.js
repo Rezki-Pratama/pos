b32b762cb639340ec6d3a1f9fa201c93c3600434
Rezki-Pratama <rezki.pratama7777@gmail.com>
2020-11-21 19:38:46 +0700

    install package and create ui login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
+import Login from './components/Login';
 import logo from './logo.svg';
-import './App.css';
+import './styles/tailwind.output.css';
+import './styles/style.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gradient-to-tr from-teal-100 to-teal-300">
+      <Login/>
     </div>
   );
 }
